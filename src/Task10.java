b7d9d9ff02d273a8beef30d31e3803b0f38a18a1
kosmonafte <itrzn@mail.ru>
2023-06-25 21:32:38 +0300

    full

--- src/Task10.java
+++ src/Task10.java
@@ -3,11 +3,21 @@ import java.util.Scanner;
 public class Task10 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int numOne = scanner.nextInt()  ;
+        int numOne = scanner.nextInt();
         int numTwo = scanner.nextInt();
         int mask = 1;
-        for (int i = 31; i >= 0; i--) {
-
+        for (int i = 30; i >= 0; i--) {
+            if (((numOne >> i) & mask) == ((numTwo >> i) & mask)) {
+                continue;
+            } else {
+                if (((numOne >> i) & mask) == 0) {
+                    System.out.println(numOne);
+                    break;
+                } else {
+                    System.out.println(numTwo);
+                    break;
+                }
+            }
         }
     }
 }
--- src/Task12.java
+++ src/Task12.java
@@ -1,20 +1,22 @@
-import com.sun.java.swing.plaf.windows.WindowsTextAreaUI;
-
 import java.util.Scanner;
 
-public class Task11 {
+public class Task12 {
     public static void main(String[] args) {
         int i = 31;
         int mask = 1;
         Scanner scanner = new Scanner(System.in);
         int input = scanner.nextInt();
-        int count = 0;
         for (;i >= 0; i--) {
             if ((input>>i&mask) == 1) {
-                count++;
+                break;
+            }
+        }
+        for (int j = i; j >= 0; j--) {
+            if ((input>>j&mask) == 0) {
+                System.out.println("Есть");
+                break;
             }
         }
         System.out.println(Integer.toBinaryString(input));
-        System.out.println(count);
     }
 }
